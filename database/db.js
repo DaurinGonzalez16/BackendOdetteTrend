52f548d6afa63ebd035edb6ea9b7847bddde301b
Daurin Gonzalez <133472346+DaurinGonzalez16@users.noreply.github.com>
2024-01-14 02:53:49 -0400

    Agregando de manera global sequelize

--- database/db.js
+++ database/db.js
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
 import { DB_HOST, DB_NAME, DB_PASSWORD, PORT, USER } from "./config.js";
 
-const db = new Sequelize(DB_NAME, USER, DB_PASSWORD, {
+const db = new Sequelize({
+  database: DB_NAME,
+  username: USER,
+  password: DB_PASSWORD,
   host: DB_HOST,
   port: PORT,
   dialect: "mysql",
