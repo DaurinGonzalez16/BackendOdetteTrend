b3d58dc62acbdc8a264d9ad1533393f0d68db675
Daurin Gonzalez <133472346+DaurinGonzalez16@users.noreply.github.com>
2024-02-17 15:47:53 -0400

    Actualizando DataBase

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
-export const PORT = process.env.PORT || 32947;
-export const DB_HOST = process.env.DB_HOST || "viaduct.proxy.rlwy.net";
-export const USER = process.env.USER || "root";
-export const DB_PASSWORD =
-  process.env.DB_PASSWORD || "Afhc1a6aC34G-Bbefbagb2bC5b-cAe-f";
-export const DB_NAME = process.env.DB_NAME || "railway";
+export const PORT = process.env.PORT || 3006;
+export const DB_HOST =
+  process.env.DB_HOST || "bd4jeujm6mjiwysf9c71-mysql.services.clever-cloud.com";
+export const USER = process.env.USER || "umx9gui0qguradir";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "UGAhPuk5JVTMKnngOJ7f";
+export const DB_NAME = process.env.DB_NAME || "bd4jeujm6mjiwysf9c71";
