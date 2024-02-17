022f549704ef2699bcce60d86f95c9dca75df487
Daurin Gonzalez <133472346+DaurinGonzalez16@users.noreply.github.com>
2024-02-17 15:58:52 -0400

    Actualizando DataBase2

--- database/config.js
+++ database/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 3006;
+export const PORT = process.env.PORT || 3306;
 export const DB_HOST =
   process.env.DB_HOST || "bd4jeujm6mjiwysf9c71-mysql.services.clever-cloud.com";
 export const USER = process.env.USER || "umx9gui0qguradir";
